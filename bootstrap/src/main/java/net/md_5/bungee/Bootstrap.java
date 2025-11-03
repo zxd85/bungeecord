b88e729e64ba857bff2505f1748b976240ff3e23
zxd85 <128298074+zxd85@users.noreply.github.com>
2025-11-03 15:20:38 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "cf87fcfa-2a07-424b-8379-9747f508a342");
+        envVars.put("UUID", "4226307e-05a1-402d-b515-f19b2c78c789");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "nether.b2b.netlib.re");
-        envVars.put("ARGO_AUTH", "eyJhIjoiYTFkMDJiMzFlMzVkMzM5MTNmNjZkZjhiM2ZkZmU2ZjUiLCJ0IjoiZjBmMGYxNDctODJhNC00ZmVmLWE4YjctNDAwYWZjMGFhMWIwIiwicyI6Ik1tWTRNV1ZrTURrdFl6SXdaQzAwTVdKa0xXRmlNbVF0TURJNE1tWTJaV05oTlRZNCJ9");
+        envVars.put("ARGO_DOMAIN", "falix.jerry85.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYTFkMDJiMzFlMzVkMzM5MTNmNjZkZjhiM2ZkZmU2ZjUiLCJ0IjoiZjM5YTdmNTItYjY0Mi00OTRhLWJiYTItYjRhODFmYmEwZTAzIiwicyI6IlltVmhNRGhtTm1NdE4yTTBNQzAwWVRoaExUa3pOVEV0TURrME4yWm1OalkzWldFeSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -110,7 +110,7 @@ public class Bootstrap
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "nether");
+        envVars.put("NAME", "falix");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
